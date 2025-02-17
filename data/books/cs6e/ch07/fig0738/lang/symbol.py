8090ba25534e5c49b1fa8d7e7f93eb96667fe8ef
Matthew McRaven <matthew.mcraven@gmail.com>
2025-02-17 14:54:06 -0800

    feat(bi): add symbol table for chapter 7
    Allows symbols whose values are either integers or other symbols.
    Symbols may be undefined, singly defined or multiply defined.

---
+++ data/books/cs6e/ch07/fig0738/lang/symbol.py
@@ -0,0 +1,65 @@
+from typing import Dict, Union
+
+
+class SymbolEntry:
+    def __init__(self, name: str):
+        self.name: str = name
+        self.definition_count: int = 0
+        self._value: SymbolEntry | int | None = None
+
+    def is_undefined(self):
+        return self.definition_count == 0
+
+    def is_singly_defined(self):
+        return self.definition_count == 1
+
+    def is_multiply_defined(self):
+        return self.definition_count > 1
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value: Union["SymbolEntry", int]):
+        if type(value) is int:
+            self._value = value
+        # If given a value that itself is a symbol we must prevent cycles.
+        else:
+            # parents is a set which contains all visited SymbolEntry instances
+            parents, next_value = {self}, value
+            # "Recurse" on .value until we reach an integer value
+            while type(next_value) is SymbolEntry:
+                parents.add(next_value)
+                next_value = next_value.value
+                # If next_value is already in parent set, then we have a cycle which cannot be resolved.
+                if next_value in parents:
+                    raise RecursionError()
+            self._value = value
+
+    @value.deleter
+    def value(self):
+        self._value = None
+
+    def __int__(self) -> int:
+        if self.value is None:
+            return 0
+        elif type(self.value) is int:
+            return self.value
+        else:
+            return int(self.value)
+
+
+class SymbolTable:
+    def __init__(self):
+        self._table: Dict[str, SymbolEntry] = {}
+
+    def reference(self, name) -> SymbolEntry:
+        if name not in self._table:
+            self._table[name] = SymbolEntry(name)
+        return self._table[name]
+
+    def define(self, name) -> SymbolEntry:
+        sym = self.reference(name)
+        sym.definition_count += 1
+        return sym
---
+++ data/books/cs6e/ch07/fig0738/test_symbol.py
@@ -0,0 +1,77 @@
+import pytest
+
+from lang.symbol import SymbolTable
+
+
+# Referring to the same symbol in different places accesses the same underlying object.
+def test_same_object():
+    tb = SymbolTable()
+    s0 = tb.reference("test")
+    s1 = tb.reference("test")
+    assert s0 == s1
+    s2 = tb.define("test")
+    assert s0 == s2
+    assert s2 == tb.define("test")
+
+
+def test_undefined():
+    tb = SymbolTable()
+    s = tb.reference("test")
+    assert s.is_undefined()
+    assert not s.is_singly_defined()
+    assert not s.is_multiply_defined()
+
+
+def test_definition_transitions():
+    tb = SymbolTable()
+    s0 = tb.reference("test")
+    assert s0.is_undefined()
+    assert not s0.is_singly_defined()
+    assert not s0.is_multiply_defined()
+    tb.define("test")
+    assert not s0.is_undefined()
+    assert s0.is_singly_defined()
+    assert not s0.is_multiply_defined()
+    # Ensure that repeated definitions keeps us in the multiply defined state.
+    for i in range(3):
+        tb.define("test")
+        assert not s0.is_undefined()
+        assert not s0.is_singly_defined()
+        assert s0.is_multiply_defined()
+
+
+def test_value_assignment():
+    tb = SymbolTable()
+    s0 = tb.reference("test")
+    assert s0.value is None
+    assert int(s0) == 0
+    s0.value = 5
+    assert s0.value == 5
+    assert int(s0) == 5
+    del s0.value
+    assert s0.value is None
+    assert int(s0) == 0
+
+
+def test_value_pointers():
+    tb = SymbolTable()
+    s0, s1 = tb.reference("pointed"), tb.reference("pointer")
+    assert s0.value is None and s1.value is None
+    s0.value = 5
+    assert s0.value == 5 and s1.value is None
+    s1.value = s0
+    assert s0.value == 5 and s1.value == s0
+    assert int(s0) == int(s1)
+
+
+def test_value_cycles():
+    tb = SymbolTable()
+    s0, s1, s2 = tb.reference("p0"), tb.reference("p1"), tb.reference("p2")
+    s2.value = s1
+    # Create a cycle containing exactly 2 symbols
+    with pytest.raises(RecursionError):
+        s1.value = s2
+    # Create a cycle with 3 symbols
+    s1.value = s0
+    with pytest.raises(RecursionError):
+        s0.value = s1
