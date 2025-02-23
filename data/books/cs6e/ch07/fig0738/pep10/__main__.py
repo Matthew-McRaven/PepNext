d0cf7952a579cf9b1f8d132aba3ae45e797fcd99
Matthew McRaven <matthew.mcraven@gmail.com>
2025-02-23 10:40:06 -0800

    feat(bi): add CLI assembler
    Still need to figure out how I want to output source, object, and listing.
    Maybe parameters who default to the name of the input file with extensions replaced?

---
+++ data/books/cs6e/ch07/fig0738/pep10/__main__.py
@@ -0,0 +1,44 @@
+import argparse
+import sys
+
+from pep10.code_gen import (
+    generate_code,
+    program_object_code,
+    program_listing,
+    program_source,
+)
+from pep10.ir import ErrorNode
+from pep10.parser import parse
+from pep10.symbol import SymbolTable, add_OS_symbols
+
+
+def assemble(text: str):
+    st = SymbolTable()
+    add_OS_symbols(st)
+    parse_tree = parse(text, symbol_table=st)
+    parse_errors = list(filter(lambda n: type(n) == ErrorNode, parse_tree))
+    if len(parse_errors) > 0:
+        for error in parse_errors:
+            print(error, file=sys.stderr)
+        raise SyntaxError("Failed to parse program")
+    ir, ir_errors = generate_code(parse_tree)
+    if len(ir_errors) > 0:
+        for ir_error in ir_errors:
+            print(ir_error, file=sys.stderr)
+        raise SyntaxError("Failed to generate object code")
+    return program_object_code(ir), program_listing(ir), program_source(ir)
+
+
+def main():
+    # Call with no arguments to open the GUI, or with a single file argument to run in the terminal.
+    parser = argparse.ArgumentParser(description="Fig 07.38")
+    parser.add_argument("input_file", default=None)
+    args = parser.parse_args()
+    if args.input_file:
+        with open(args.input_file, "r") as f:
+            object_code, listing, source = assemble("".join(f.readlines()))
+            print("\n".join(listing))
+
+
+if __name__ == "__main__":
+    main()
